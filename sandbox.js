a90bbd60c34fddafe1388c760f6e1a1e4e6c0d4d
ilkin <huseynlilkin@gmail.com>
2021-11-21 23:32:57 +0100

    4 - arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -3,11 +3,24 @@
  * - Compile by running : tsc sandbox.ts [sandbox.js]
  * - Continious compile : tsc sandbox.ts -w
  */
-// TS can infer type from first assigned value
-var age = 30;
-age = 2 / 3;
-// defining argument type
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['robert', 'downey'];
+names.push('jr');
+// not allowed
+// names.push(3);
+// will put empty to index 3 and 4
+names[5] = 'tom';
+console.log(names);
+// defining mixed array. You can only push types are already available
+var mixed = ['ken', 4, 'str2', 5, false];
+mixed.push(true);
+// Objects: Once initially decalared, can't change structure and types
+var ninja = {
+    name: 'mario',
+    belt: 'yellow',
+    age: 30
 };
-console.log(circ(4));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '50'
+// name.skills = ['fighting']
--- sandbox.ts
+++ sandbox.ts
@@ -4,13 +4,29 @@
  * - Continious compile : tsc sandbox.ts -w
  */
 
-// TS can infer type from first assigned value
-let age = 30;
-age = 2/3;
+// arrays
+let names = ['robert', 'downey']
+names.push('jr');
 
-// defining argument type
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-}
+// not allowed
+// names.push(3);
 
-console.log(circ(4));
+// will put empty to index 3 and 4
+names[5] = 'tom';
+console.log(names);
+
+// defining mixed array. You can only push types are already available
+let mixed = ['ken', 4, 'str2', 5, false]
+mixed.push(true);
+
+// Objects: Once initially decalared, can't change structure and types
+let ninja = {
+    name: 'mario',
+    belt: 'yellow',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '50'
+// name.skills = ['fighting']
